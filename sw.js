391b8e87459fa46510f03ca0ed6a92eb63e0ed21
Nick Page <pagens@btinternet.com>
2026-09-13 18:10:55 +0000

    Bump service worker cache version so redesign reaches installed home screen app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hottub-test-v1";
+const CACHE = "hottub-test-v2";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
